ca7a65ae999f129848667d9eb28ed693df84839f
Tony <greybluesea@gmail.com>
2023-08-13 22:39:53 +1200

    yet minor

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -8,7 +8,7 @@ const Footer = (props: Props) => {
       <ul className="hidden fixed bottom-20 lg:flex lg:flex-col mt-auto  mx-auto  justify-end items-center text-gray-500 bg-transparent space-y-1 px-7 py-5 rounded-xl w-160">
         <li>NextAuth + Email Verification</li>
         <li>Prisma + Postgres + schema migrations for change</li>
-        <li>customized "session" object + server action for Register form</li>
+        <li>customized session object + server action for Register form</li>
         <li>
           Learned from Sakura Dev, Antonio, Ethan Mick, & Onelight Web Dev
         </li>
