e8962d56e72524f8be740e32ef08a44f4e608a16
Tony <greybluesea@gmail.com>
2023-08-14 17:56:54 +1200

    schema changed to 'onDelete: Cascade' successful

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -6,8 +6,8 @@ const Footer = (props: Props) => {
   return (
     <footer className=" none lg:fixed bottom-16 flex flex-row justify-center">
       <ul className="hidden fixed bottom-20 lg:flex lg:flex-col mt-auto  mx-auto  justify-end items-center text-gray-500 bg-transparent space-y-1 px-7 py-5 rounded-xl w-160">
-        <li>NextAuth + Email Verification(NodeMailer)</li>
-        <li>Prisma + Postgres + schema migrations for change</li>
+        <li>NextAuth + email verification(NodeMailer)</li>
+        <li>Prisma + PostgreSQL + schema migrations for change</li>
         <li>customized ‘session’ object + server action for Register form</li>
         <li>learned from Sakura Dev, Antonio, & Ethan Mick</li>
         <li>powered by greybluesea</li>
